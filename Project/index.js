b34b879d5ceaccf4650c7f676ea4386864b7133f
Dominik Krotzer <dokr2508@colorado.edu>
2022-11-04 14:38:11 -0600

    Added Lab11 document in milestone folder

---
+++ Project/index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+const pgp = require("pg-promise")();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+// db config
+const dbConfig = {
+  host: "db",
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+
+const db = pgp(dbConfig);
+
+// db test
+db.connect()
+  .then((obj) => {
+    // Can check the server version here (pg-promise v10.1.0+):
+    console.log("Database connection successful");
+    obj.done(); // success, release the connection;
+  })
+  .catch((error) => {
+    console.log("ERROR:", error.message || error);
+  });
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+
+// set session
+app.use(
+  session({
+    secret: "XASDASDA",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', (req,res) => //Homepage
+{
+
+}) 
+
+app.get('/login', (req,res) => //Load Login page
+{
+
+})
+
+app.post('/login', (req,res) =>
+{
+    
+})
+
+app.get('/register', (req,res) => //Register
+{
+
+})
+
+app.post('/register', (req,res) =>
+{
+    
+})
+
+app.post('/addtrail', (req,res) =>
+{
+    
+})
+app.post('/addfriend', (req,res) =>
+{
+    
+})
+
+app.get('/findtrails', (req,res) =>
+{
+    
+})
+
+app.get('/findfriends', (req,res) =>
+{
+    
+})
+
+app.get('/messages', (req,res) =>
+{
+
+})
