7922f88f0bb4a6a8f063ebc4cce6cf5cd45891d5
nosc7618 <nosc7618@colorado.edu>
2022-11-27 20:43:42 -0700

    revert to Matthews change

--- Project/index.js
+++ Project/index.js
@@ -161,13 +161,9 @@ app.post('/updateProfile', async (req,res)=>
   const user_bio=req.body.user_bio;
   const user_city=req.body.user_city;
   //fix here Where username = ?
-  const query = `UPDATE Users SET user_bio=$1, user_city=$2 WHERE username=bbbbbb`;
-  await db.any(query,[username])
+  const query = `UPDATE Users SET user_bio=$2, user_city=$3 WHERE username=$1`;
+  await db.any(query,[username, user_bio, user_city])
     .then(function (data) {
-      //assign const user updated valuesS
-        // user.username=username;
-        // user.user_bio=user_bio;
-        // user.user_city=user_city;
         res.redirect("/displayUserProfile");
         })
     .catch(function (err) {
