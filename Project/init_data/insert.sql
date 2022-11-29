c695fd6b4b2b367b9c8ee8a16fa4ec02441d026c
Zach Cook <zaco6003@colorado.edu>
2022-11-28 22:26:29 -0700

    got some sample users added

--- Project/init_data/insert.sql
+++ Project/init_data/insert.sql
@@ -4,3 +4,13 @@ INSERT INTO routes (route_id, route_name, route_city, rating) VALUES
 ('3', 'The Boulder Carnival Wall', 'Boulder', '1'),
 ('4', 'Fort Collins Gym', 'Fort Collins', '5'),
 ('5', 'Golden Rock Wall', 'Golden', '6');
+
+INSERT INTO users (username, password, user_bio, user_city) VALUES
+('climb.bradley', 'bjlfiobtj', 'I love to climb mountains, rock climbing, and ice climbing. Ive climbed all over the world, from Yosemite to Elbrus to Annapurna.', 'Cheyenne'),
+('clm10', 'bvijoreiore', 'I also enjoy hiking, camping, and exploring nature. ', 'Boulder'),
+('therock' , 'dfklrgkl', 'When im not fast or furious, im climbing', 'Los Angleles'),
+('turbo_jeffrey', 'sdfkbkjlfrr', 'the worlds fastest human. He can run faster than the speed of sound', 'Boulder'),
+('yogi.carl', 'fgkjgrkjlbvkln', 'Im a yoga teacher and I love helping people find their zen. Im also a bit of a foodie, and I love to cook. When Im not teaching yoga or cooking, you can find me hanging out with my cat, Mr. Tibbles.', 'Boulder'),
+('samatha.cricket' , 'dfkjlaskl', 'Getting older is like a box of chocolates: you never know what youre going to get.', 'Boulder'),
+('chad_10', 'vngrrddvcd', 'Is life attempting to bring me down? Great, Ill just use it as fuel.','Denver'),
+('testuser', '$2b$10$qGERG4UYi5wuhbPO8krTEOsPr3jZZk6gNXSTYx.mZXBdlnaw/fEBK', 'This is a sample bio you can update', 'Boulder');
